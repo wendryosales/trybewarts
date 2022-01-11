7add2aaebeb149802e63a514e4db740d39ecf4a3
Wendryo S <wendryo.sales@gmail.com>
2022-01-11 16:19:41 -0300

    Requisitos 1 ao 4 ok

---
+++ script.js
@@ -0,0 +1,15 @@
+// Validação do e-mail e senha
+
+const email = document.querySelectorAll('input')[0];
+const senha = document.querySelectorAll('input')[1];
+const botaoEnviar = document.querySelector('button');
+
+botaoEnviar.addEventListener("click", validation);
+
+function validation (){
+    if (email.value === "tryber@teste.com" && senha.value === "123456") {
+        alert("Olá, Tryber!");
+    } else {
+        alert("Email ou senha inválidos.")
+    }
+}
