3cfa10ee375877b008a73adeef2eb0e03ce4231c
Wendryo S <wendryo.sales@gmail.com>
2022-01-12 15:35:29 -0300

    ok

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Validação do e-mail e senha
-
 const email = document.querySelectorAll('input')[0];
 const senha = document.querySelectorAll('input')[1];
 const botaoEnviar = document.querySelector('button');
