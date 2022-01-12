dd2fe5968038ad5563b98ef0f2bf8ea4c70d8981
Wendryo S <wendryo.sales@gmail.com>
2022-01-12 14:58:47 -0300

    Requisito 20 ok

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const email = document.querySelectorAll('input')[0];
 const senha = document.querySelectorAll('input')[1];
 const botaoEnviar = document.querySelector('button');
 
-
 function validation() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
@@ -16,16 +15,26 @@ function validation() {
 botaoEnviar.addEventListener('click', validation);
 
 // Botão de aceitar termos
-
-
-
-const aceitarTermos = document.querySelector('#agreement')
-const enviarForm = document.querySelector('#submit-btn')
+const aceitarTermos = document.querySelector('#agreement');
+const enviarForm = document.querySelector('#submit-btn');
 
 function acepet() {
-if(aceitarTermos.checked){
-  enviarForm.removeAttribute('disabled')
+  if (aceitarTermos.checked) {
+    enviarForm.removeAttribute('disabled');
+  }
 }
 
-}
 aceitarTermos.addEventListener('click', acepet);
+
+// Contador de caracteres
+const txtarea = document.querySelector('textarea');
+const count = document.querySelector('#counter');
+
+function contador() {
+  const inicial = 500;
+  const caracteres = txtarea.value.length;
+
+  count.innerHTML = inicial - caracteres;
+}
+
+txtarea.addEventListener('input', contador);
