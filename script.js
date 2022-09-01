7ce651d2e883d1f419f9ad0336a8887c5c61e8a4
Yuut <ingride.reis.b@gmail.com>
2022-09-01 11:57:28 -0300

    :bug: fix: Ajuste da imagem de fundo, correção de erros de lint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Validação do e-mail e senha
 const email = document.querySelectorAll('input')[0];
 const senha = document.querySelectorAll('input')[1];
-const botaoEnviar = document.querySelector('a');
+const botaoEnviar = document.querySelector('.botaozinho');
 
 function validation() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
